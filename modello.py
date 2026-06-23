75dd8752f718fc4ecb671d54568f5a90580b2e09
Kektus8000 <simy8000@gmail.com>
2026-06-23 17:10:52 +0200

    Modello + Esempi caricati

--- modello.py
+++ modello.py
@@ -86,8 +86,32 @@ def visualize(
   return annotated_image
 
 # Serve a mostrare un'immagine tramite cv2
-IMAGE_FILE = 'fotoImportante.jpg'
-img = cv2.imread(IMAGE_FILE)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
 
+IMAGE_FILE = 'img/fotoGruppo.jpg'
+#img = cv2.imread(IMAGE_FILE)
+#cv2.imshow("Image", img)
+#cv2.waitKey(0)
+
+# STEP 1: Import the necessary modules.
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an FaceDetector object.
+base_options = python.BaseOptions(model_asset_path='detector.tflite')
+options = vision.FaceDetectorOptions(base_options=base_options)
+detector = vision.FaceDetector.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file(IMAGE_FILE)
+
+# STEP 4: Detect faces in the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the detection result. In this case, visualize it.
+image_copy = np.copy(image.numpy_view())
+annotated_image = visualize(image_copy, detection_result)
+rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+cv2.imshow("Image", rgb_annotated_image)
+cv2.waitKey(0)
