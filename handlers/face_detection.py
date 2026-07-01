b1b3ac5bf9ae73305eb6697ea14d055e88a83a6a
Flavio Corsetty <flavio.corsetti@yahoo.it>
2026-07-01 09:51:56 +0200

    modifica

--- handlers/face_detection.py
+++ handlers/face_detection.py
@@ -1,29 +1,28 @@
-import modello as md
-import json
+from . import modello as md
 import boto3
 
 DESTINATION_BUCKET = "model-processing-images-output"
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')   # <-- client, non resource
 
 def lambda_handler(event, context):
     bucket_input = event['Records'][0]['s3']['bucket']['name']
     input_key = event['Records'][0]['s3']['object']['key']
-    
+
     img_obj = s3.get_object(Bucket=bucket_input, Key=input_key)
     img = img_obj['Body'].read()
 
     returned_image = md.analyze_img(img)
-    if returned_image == False:
+    if returned_image is False:
         return {
             'statusCode': 400,
             'body': 'The uploaded file was not an image'
         }
-        
+
     s3.put_object(
-        Bucket= DESTINATION_BUCKET,
-        Key= input_key,
-        Body= returned_image,
-        ContentType='image'
+        Bucket=DESTINATION_BUCKET,
+        Key=input_key,
+        Body=returned_image,
+        ContentType='image/jpeg'   # <-- 'image' da solo non è un mimetype valido
     )
 
     return {'statusCode': 200}
