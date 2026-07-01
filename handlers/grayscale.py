e28cab33e1889a6a2a55e2bfe225159a48571151
Flavio Corsetty <flavio.corsetti@yahoo.it>
2026-07-01 18:04:50 +0200

    graymod

--- handlers/grayscale.py
+++ handlers/grayscale.py
@@ -4,7 +4,7 @@ import numpy as np
 import mediapipe as mp
 from PIL import Image
 
-def grayscale_img(file_bytes : bytes): 
+def grayscale_img(file_bytes: bytes):
     # Verifica che sia un'immagine
     try:
         with Image.open(io.BytesIO(file_bytes)) as img:
@@ -18,20 +18,9 @@ def grayscale_img(file_bytes : bytes):
         f.write(file_bytes)
 
     img = mp.Image.create_from_file(tmp_path)
-    image_copy = np.copy(img.numpy_view())
-    gray_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB)    
+    image_copy = np.copy(img.numpy_view())  # formato RGB
+    gray_image = cv2.cvtColor(image_copy, cv2.COLOR_RGB2GRAY)  # ora è vera scala di grigi
 
     # Converti in bytes per S3
     _, buffer = cv2.imencode(".jpg", gray_image)
     return buffer.tobytes()
-
-"""
-def lambda_handler(event, context):
-    img_bytes = download_image(event["source_bucket"], event["source_key"])
-    with Image.open(io.BytesIO(img_bytes)) as img:
-        grayscale = img.convert("L")
-        out = io.BytesIO()
-        grayscale.save(out, format=img.format or "JPEG")
-    upload_image(event["dest_bucket"], event["dest_key"], out.getvalue())
-    return {"statusCode": 200, "body": f"Grayscale salvato → {event['dest_key']}"}
-"""
