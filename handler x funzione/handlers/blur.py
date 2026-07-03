f8031c848b450428b4f4d1017dea279ead858277
Flavio Corsetty <flavio.corsetti@yahoo.it>
2026-07-03 18:37:46 +0200

    Create blur.py

---
+++ handler x funzione/handlers/blur.py
@@ -0,0 +1,49 @@
+import boto3
+import cv2
+import io
+import numpy as np
+import mediapipe as mp
+from PIL import Image
+
+BLUR_SIZE = (30, 30)
+DESTINATION_BUCKET = "model-processing-images-output"
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket_input = event['Records'][0]['s3']['bucket']['name']
+    input_key = event['Records'][0]['s3']['object']['key']
+
+    img_obj = s3.get_object(Bucket=bucket_input, Key=input_key)
+    file_bytes = img_obj['Body'].read()
+
+    # Verifica che sia un'immagine
+    try:
+        with Image.open(io.BytesIO(file_bytes)) as img:
+            img.verify()
+    except (IOError, SyntaxError):
+        return {
+            'statusCode': 400,
+            'body': 'The uploaded file was not an image'
+        }
+
+    # Salva temporaneamente su /tmp (unico path scrivibile in Lambda)
+    tmp_path = "/tmp/input_image.jpg"
+    with open(tmp_path, "wb") as f:
+        f.write(file_bytes)
+
+    img = mp.Image.create_from_file(tmp_path)
+    image_copy = np.copy(img.numpy_view())  # formato RGB
+    image_copy = cv2.cvtColor(image_copy, cv2.COLOR_RGB2BGR)
+    blur_image = cv2.blur(image_copy, BLUR_SIZE)
+
+    # Converti in bytes per S3
+    _, buffer = cv2.imencode(".jpg", blur_image)
+
+    # Carica l'immagine nel bucket di destinazione
+    s3.put_object(
+        Bucket=DESTINATION_BUCKET,
+        Key=input_key,
+        Body=buffer.tobytes(),
+        ContentType='image/jpeg'
+    )
+    return {'statusCode': 200}
